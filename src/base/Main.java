ab8f68aa41cb83e1ff8930b9c59c24856254eb32
Nick <nh02173@georgiasouthern.edu>
2014-09-10 22:34:18 -0400

    Tweaked logging again. Updated test comments so that they are specific to the assignment requirements. Added test code for AccessOps2

--- src/base/Main.java
+++ src/base/Main.java
@@ -22,7 +22,7 @@ public class Main {
         BagCollection<String> test1 = new BagCollection<String>();
 
         // Pre-Add
-        System.out.println("-The current state is isEmpty=" + test1.isEmpty());
+        System.out.println(">>The current state is isEmpty=" + test1.isEmpty());
 
         // Add beyond 10 to test auto-expansion
         for (int index = 0; index < sampleSize1; index++) {
@@ -30,14 +30,13 @@ public class Main {
         }
 
         // Post-Add
-        System.out.println("-The current state is isEmpty=" + test1.isEmpty());
+        System.out.println(">>The current state is isEmpty=" + test1.isEmpty());
 
         // Should be 10 + sampleSize1
-        System.out.println("-The current number of items is " + test1.size());
+        System.out.println(">>The current number of items is " + test1.size());
 
         /* Test with numbers
         - Test constructor w/parameter
-        - Test getMax()
         - Test contains(T)
         - Test remove(T)
         - Test removeRandom()
@@ -60,30 +59,50 @@ public class Main {
 
         /* Test collections (matching types)
         - Test union() on empty collection
-        - Test addAll()
         - Test union() on non-empty collection
+        - Test addAll()
         - Test equals() on matching collection
         - Test equals() on non-matching collection
          */
 
+        int sampleSize3 = 2;
 
+        // Original
+        BagCollection<Integer> test3 = new BagCollection<Integer>(sampleSize3);
 
-        /* Test collections (disparate types)
-        - Test union() (exception)
-        - Test addAll() (exception)
-        - Test equals() (false)
-         */
+        // Add some numbers
+        for (int index = 0; index < sampleSize3; index++) {
+            // Use wider bounds if using a small sample set
+            test3.add(getRandomInt(100));
+        }
 
+        // Union an empty instance to create a new collection (Simulates a copy operation)
+        BagCollection<Integer> test4 = test3.union(new BagCollection<Integer>(0));
 
+        // Union the copy with original (Simulates an doubling of the collection)
+        BagCollection<Integer> test5 = test3.union(test4);
 
+        // Add together the copy and the original. (Make the original a second double by adding the clone to it)
+        // This instance will also expand on demand making its max differ from the union result.
+        test3.addAll(test4);
+
+        // Compare the two doubles
+        System.out.println(test3.equals(test5));
+
+        // Compare the clone and one of the doubles
+        System.out.println(test3.equals(test4));
     }
 
     static int getRandomInt(int bounds) {
         Random gen = new Random();
-        if (bounds > 0) {
-            return gen.nextInt(bounds);
-        } else {
-            return gen.nextInt();
+        try {
+            if (bounds > 0) {
+                return gen.nextInt(bounds);
+            } else {
+                return gen.nextInt();
+            }
+        } finally {
+            gen=null;
         }
     }
 
